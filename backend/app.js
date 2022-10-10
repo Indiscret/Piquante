96423a5856d2648582e0d1190fcf3699cbbd219c
Saian <ifsi.seraffin@gmail.com>
2022-10-10 20:11:00 +0200

    Ajout route sauce api (model, route, contrôleur)

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require(path);
+
+const Sauce = require('./models/Sauce');
+
+const path = require('path');
 
 const app = express();
 
 const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
 
 
 mongoose.connect('mongodb+srv://Saian:1234@hottakes.lbwvgpp.mongodb.net/?retryWrites=true&w=majority',
@@ -24,6 +28,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
     
 module.exports = app;
